af25f3646db87c0d263c4914eeb9ba0d0c8bab1a
Oren Koriat <oren@cynerio.co>
2020-05-13 15:23:36 +0300

    influenza_vs_covid_by_week query adjusted to new schema

--- queries/influenza_vs_covid_by_week.sql
+++ queries/influenza_vs_covid_by_week.sql
@@ -1,7 +1,8 @@
-select datetime(substr("Start week", 7, 4) || '-' || substr("Start week", 1, 2) || '-' || substr("Start week", 4, 2)) as 'Week',
-       cast(replace("All COVID-19 Deaths (U07.1)", ',', '') as integer) as 'COVID Deaths',
-       cast(replace("All Influenza Deaths (J09-J11)", ',', '') as integer) as 'Influenza Deaths'
+select start_week as start_week,
+       cast(replace(covid_deaths, ',', '') as integer) as 'COVID Deaths',
+       cast(replace(influenza_deaths, ',', '') as integer) as 'Influenza Deaths'
 from corona_us_deaths
-where "Group" = 'By week'
-and Indicator != 'Total Deaths'
+where "group" = 'By week'
+and state = 'United States'
+order by start_week
 ;
