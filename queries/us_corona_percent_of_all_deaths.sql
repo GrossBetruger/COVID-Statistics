0b96959b7bd039ecdc7808778e15defc92a05e56
Oren Koriat <oren@cynerio.co>
2020-05-16 15:32:29 +0300

    percent of all death query: update to new schema

--- queries/us_corona_percent_of_all_deaths.sql
+++ queries/us_corona_percent_of_all_deaths.sql
@@ -1,8 +1,10 @@
-select cast(strftime('%s', datetime(substr("Start week", 7, 4) || '-' || substr("Start week", 1, 2) || '-' || substr("Start week", 4, 2))) as int) as 'timestamp',
-       datetime(substr("Start week", 7, 4) || '-' || substr("Start week", 1, 2) || '-' || substr("Start week", 4, 2)) as 'Week',
-       round(cast(replace("All COVID-19 Deaths (U07.1)", ',', '') as integer) * 1.0 /
-       cast(replace("Deaths from All Causes", ',', '') as integer) * 100, 2) as 'Percent of Deaths'
+select start_week,
+       round(cast(replace(covid_deaths, ',', '') as integer) * 1.0 /
+       cast(replace(total_deaths, ',', '') as integer) * 100, 2) as 'Percent of Deaths',
+       covid_deaths,
+       total_deaths
 from corona_us_deaths
 where "Group" = 'By week'
-and Indicator != 'Total Deaths'
+and state = 'United States'
 ;
+
