a3a7c012c6207a35b31ff432f488d81f70cfe966
Oren Koriat <oren@cynerio.co>
2021-01-24 16:04:04 +0200

    field names

--- queries/influenza_vs_covid_by_week.sql
+++ queries/influenza_vs_covid_by_week.sql
@@ -1,5 +1,5 @@
-select cast(influenza_deaths as integer),
-       cast(covid_19_deaths as integer),
+select cast(influenza_deaths as integer) "Influenza Deaths",
+       cast(covid_19_deaths as integer) "COVID-19 Deaths",
        datetime(end_date) "week (end date)"
 from corona_us_deaths
 where state = 'United States'
