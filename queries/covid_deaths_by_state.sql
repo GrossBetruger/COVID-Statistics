8882a9a55ab0e0015594c54b4e856e0ea9f0c3c8
Oren Koriat <oren@cynerio.co>
2021-02-02 18:52:48 +0200

    update death by state query

--- queries/covid_deaths_by_state.sql
+++ queries/covid_deaths_by_state.sql
@@ -1,13 +1,7 @@
-with agg_new_york as (
-    select
-    case when  state like  'New York%' then 'New York' else state end as state,
-    covid_deaths
-    from corona_us_deaths
-)
-select sum(covid_deaths) covid_deaths_in_state, state
-from agg_new_york
+select sum(covid_19_deaths) "COVID-19 Deaths", state "State"
+from corona_us_deaths
 where state != 'United States'
 group by state
-order by covid_deaths_in_state desc
+order by "COVID-19 Deaths" desc
 ;
 
