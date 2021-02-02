9d7b8b31a90cce97e5c7cff340850c4390ba5a63
Oren Koriat <oren@cynerio.co>
2021-02-02 19:02:54 +0200

    update age groups question

--- queries/us_deaths_age_distribution.sql
+++ queries/us_deaths_age_distribution.sql
@@ -1,7 +1,7 @@
-select age_group,
+select age_group_new age_group,
        round(cast(covid_19_deaths as float) / total_deaths, 4) * 100 as 'percent of all deaths',
-       covid_19_deaths,
-       total_deaths
+       cast(covid_19_deaths as intger),
+       cast(total_deaths as integer)
 from us_corona_deaths_age_and_sex
 where state = 'United States'
 and sex = 'All Sexes'
